b24c427d089f09c7de4631247c58f68a40172d6b
brutalID <110870412+brutalGH@users.noreply.github.com>
2023-06-03 12:55:26 +0700

    Add files via upload

---
+++ backup/set-br.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+#IZIN=$( curl http://autoscript.totalh.net/izin | grep $MYIP )
+if [ $MYIP = $MYIP ]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+else
+echo -e "${NC}${RED}Permission Denied!${NC}";
+echo -e "${NC}${LIGHT}Please Contact Admin!!"
+echo -e "${NC}${LIGHT}WhatsApp : 082241967317"
+echo -e "${NC}${LIGHT}Youtube : youtube.com/@franata775"
+echo -e "${NC}${LIGHT}Telegram : https;//t.me/f75_store"
+exit 0
+fi
+# Link Hosting Kalian
+akbarvpn="raw.githubusercontent.com/brutalGH/mini/main/backup"
+
+curl https://rclone.org/install.sh | bash
+printf "q\n" | rclone config
+wget -O /root/.config/rclone/rclone.conf "https://${akbarvpn}/rclone.conf"
+git clone  https://github.com/magnific0/wondershaper.git
+cd wondershaper
+make install
+cd
+rm -rf wondershaper
+echo > /home/limit
+apt install msmtp-mta ca-certificates bsd-mailx -y
+cat<<EOF>>/etc/msmtprc
+defaults
+tls on
+tls_starttls on
+tls_trust_file /etc/ssl/certs/ca-certificates.crt
+
+account default
+host smtp.gmail.com
+port 587
+auth on
+user anuybazoelk639@gmail.com
+from anuybazoelk639@gmail.com
+password 25Agustus@1990 
+logfile ~/.msmtp.log
+EOF
+chown -R www-data:www-data /etc/msmtprc
+cd /usr/bin
+wget -O autobackup "https://${akbarvpn}/autobackup.sh"
+wget -O backup "https://${akbarvpn}/backup.sh"
+wget -O restore "https://${akbarvpn}/restore.sh"
+wget -O strt "https://${akbarvpn}/strt.sh"
+wget -O limitspeed "https://${akbarvpn}/limitspeed.sh"
+chmod +x autobackup
+chmod +x backup
+chmod +x restore
+chmod +x strt
+chmod +x limitspeed
+cd
+rm -f /root/set-br.sh
